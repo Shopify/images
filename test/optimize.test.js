0e45a98a8d9d0e456e8796c19a032a5c94ee349c
Alexandre Côté <alex.cote@shopify.com>
2020-03-25 10:13:47 -0400

    Fix the broken test + bump every dependencies

--- test/optimize.test.js
+++ test/optimize.test.js
@@ -1,27 +1,15 @@
-/* eslint-disable shopify/strict-component-boundaries */
 import SVGO from 'svgo';
+
 import {svgOptions} from '../optimize';
+
 import {getFixture} from './helpers';
 
 describe('optimize()', () => {
   describe('svg', () => {
     it('removes all useless attributes', async () => {
-      const content = await optimize(getFixture('basic'));
-      expect(content).not.toMatch(/title/);
+      const svgo = new SVGO(svgOptions());
+      const {data} = await svgo.optimize(getFixture('basic'));
+      expect(data).not.toMatch(/title/);
     });
   });
 });
-
-function optimize(source, options) {
-  const svgo = new SVGO(svgOptions(options));
-
-  return new Promise((resolve, reject) => {
-    svgo.optimize(source, (result) => {
-      if (result.error) {
-        return reject(new Error(result.error));
-      } else {
-        return resolve(result.data);
-      }
-    });
-  });
-}
